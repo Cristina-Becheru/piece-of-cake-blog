3e9c0c54524ccdbde053a4ef4bd23c32a8f14102
Cristina-Alexandra Becheru <cristina_becheru@yahoo.com>
2024-08-02 11:38:51 +0000

    Initialize the Recipe app structure.

---
+++ recipes/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Recipe
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "cake_type",
+        "flavor",
+        "calories",
+        "posted_date",
+        "image",
+    )
+    list_filter = ("cake_type", "flavor")
+    search_fields = ("title", "description", "ingredients")
+    ordering = ("-posted_date",)
+
